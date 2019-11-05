193dad812f70ec966e75c956218601c88ce2f85d
Leioy <13902303081@139.com>
2019-11-05 16:02:45 +0800

    创建icon组件

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Button from './button'
+import Icon from './icon'
 
-ReactDOM.render(<Button />, document.body)
-console.log(React);
-console.log(ReactDOM);
-console.log(Button);
+ReactDOM.render(<div>
+  <Icon name="wechat" />
+</div>, document.body)
