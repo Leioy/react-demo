09bb6e758c82f05475330df29e9b73fe5eb78f4f
Leioy <13902303081@139.com>
2019-11-06 19:14:33 +0800

    初步完成icon

---
+++ lib/icon/icon.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './importAll.js'
+import './icon.scss'
+import classes from '../helpers/classes'
+interface IconProps extends React.SVGAttributes<SVGElement> {
+  name: string
+}
+const Icon: React.FunctionComponent<IconProps> = ({ className, name, ...restProps }) => {
+  return (
+    <svg className={classes('rui-icon', className)} {...restProps}>
+      <use xlinkHref={`#${name}`}></use>
+    </svg>
+  )
+}
+export default Icon
--- lib/index.tsx
+++ lib/index.tsx
@@ -1,7 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Icon from './icon'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Icon from './icon/icon'
 
+const fn: React.MouseEventHandler = (e) => {
+  console.log(e.target)
+}
 ReactDOM.render(<div>
-  <Icon name="wechat" />
-</div>, document.body)
+  <Icon className="icon" name="wechat" onClick={fn} onMouseEnter={() => console.log('enter')} />
+</div>, document.querySelector('#root'))
