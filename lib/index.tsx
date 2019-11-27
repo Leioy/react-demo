3604f06e0f0818232d7303c86da4685a34b146c5
Leioy <13902303081@139.com>
2019-11-27 23:00:47 +0800

    更改index.tsx

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,10 +1,1 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import Icon from './icon/icon'
-
-const fn: React.MouseEventHandler = (e) => {
-  console.log(e.target)
-}
-ReactDOM.render(<div>
-  <Icon className="icon" name="wechat" onClick={fn} onMouseEnter={() => console.log('enter')} />
-</div>, document.querySelector('#root'))
+export { default as Icon } from './icon/icon'
