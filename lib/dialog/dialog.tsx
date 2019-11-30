6b7f24aa6d76631b6fa37c7ce666ccfa016312ec
Leioy <13902303081@139.com>
2019-11-30 22:51:13 +0800

    使用react portal api

--- lib/dialog/dialog.tsx
+++ lib/dialog/dialog.tsx
@@ -1,6 +1,7 @@
 import React, { Fragment } from 'react'
 import './dialog.scss'
 import Icon from '../icon/icon'
+import ReactDOM from 'react-dom'
 interface IProps {
   visible: boolean
   buttons: Array<React.ReactElement>
@@ -22,23 +23,24 @@ const Dialog: React.FunctionComponent<IProps> = (props) => {
       props.onClose(e)
     }
   }
-  return (
-    props.visible ?
-      <Fragment>
-        <div className={prefixAdder('mask')} onClick={onClickMask}></div>
-        <div className={prefixAdder()}>
-          <div className={prefixAdder('close')} onClick={onClickClose}>
-            <Icon name="close" />
-          </div>
-          <header className={prefixAdder('header')}>提示</header>
-          <main className={prefixAdder('main')}>{props.children}</main>
-          <footer className={prefixAdder('footer')}>
-            {props.buttons.map((button, index) => React.cloneElement(button, { key: index }))}
-          </footer>
+  const x = props.visible ?
+    <Fragment>
+      <div className={prefixAdder('mask')} onClick={onClickMask}></div>
+      <div className={prefixAdder()}>
+        <div className={prefixAdder('close')} onClick={onClickClose}>
+          <Icon name="close" />
         </div>
-      </Fragment>
-      :
-      null
+        <header className={prefixAdder('header')}>提示</header>
+        <main className={prefixAdder('main')}>{props.children}</main>
+        <footer className={prefixAdder('footer')}>
+          {props.buttons.map((button, index) => React.cloneElement(button, { key: index }))}
+        </footer>
+      </div>
+    </Fragment>
+    :
+    null
+  return (
+    ReactDOM.createPortal(x, document.body)
   )
 }
 Dialog.defaultProps = {
