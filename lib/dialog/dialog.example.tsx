96a0a58dfcdef8a52efe7a477fb007ef1181ebbb
Leioy <13902303081@139.com>
2019-11-30 22:42:48 +0800

    给dialog添加事件

--- lib/dialog/dialog.example.tsx
+++ lib/dialog/dialog.example.tsx
@@ -2,10 +2,37 @@ import React, { useState } from 'react'
 import Dialog from './dialog'
 export default function () {
   const [x, setX] = useState(false)
-  return <div>
-    <button onClick={() => { setX(!x) }}>click</button>
-    <Dialog visible={x}>
-      <div>hi</div>
-    </Dialog>
-  </div>
+  const [y, setY] = useState(false)
+  return (
+    <div>
+      <div>
+        example1
+        <button onClick={() => { setX(!x) }}>click</button>
+        <Dialog visible={x} buttons={
+          [
+            <button onClick={() => { setX(false) }}>1</button>,
+            <button onClick={() => { setX(false) }}>2</button>
+          ]
+        }
+          onClose={() => { setX(false) }}
+        >
+          <div>hi</div>
+        </Dialog>
+      </div>
+      <div>
+        example2
+        <button onClick={() => { setY(!y) }}>click</button>
+        <Dialog closeOnClickMask={true} visible={y} buttons={
+          [
+            <button onClick={() => { setY(false) }}>1</button>,
+            <button onClick={() => { setY(false) }}>2</button>
+          ]
+        }
+          onClose={() => { setY(false) }}
+        >
+          <div>hi</div>
+        </Dialog>
+      </div>
+    </div>
+  )
 }
--- lib/dialog/dialog.tsx
+++ lib/dialog/dialog.tsx
@@ -3,6 +3,9 @@ import './dialog.scss'
 import Icon from '../icon/icon'
 interface IProps {
   visible: boolean
+  buttons: Array<React.ReactElement>
+  onClose: React.MouseEventHandler
+  closeOnClickMask?: boolean
 }
 function prefixFunctionMaker (prefix = '') {
   return function x (name?: string) {
@@ -11,24 +14,34 @@ function prefixFunctionMaker (prefix = '') {
 }
 const prefixAdder = prefixFunctionMaker('rui-dialog')
 const Dialog: React.FunctionComponent<IProps> = (props) => {
+  const onClickClose: React.MouseEventHandler = (e) => {
+    props.onClose(e)
+  }
+  const onClickMask: React.MouseEventHandler = (e) => {
+    if (props.closeOnClickMask) {
+      props.onClose(e)
+    }
+  }
   return (
     props.visible ?
       <Fragment>
-        <div className={prefixAdder('mask')}></div>
+        <div className={prefixAdder('mask')} onClick={onClickMask}></div>
         <div className={prefixAdder()}>
-          <div className={prefixAdder('close')}>
+          <div className={prefixAdder('close')} onClick={onClickClose}>
             <Icon name="close" />
           </div>
           <header className={prefixAdder('header')}>提示</header>
           <main className={prefixAdder('main')}>{props.children}</main>
           <footer className={prefixAdder('footer')}>
-            <button>ok</button>
-            <button>cancel</button>
+            {props.buttons.map((button, index) => React.cloneElement(button, { key: index }))}
           </footer>
         </div>
       </Fragment>
       :
       null
   )
+}
+Dialog.defaultProps = {
+  closeOnClickMask: false
 }
 export default Dialog
