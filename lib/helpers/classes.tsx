7352ce50e503b5e7ed306c9db396e3d9b40db168
Leioy <13902303081@139.com>
2019-12-07 20:57:34 +0800

    使用函数式重构prefixFunctionMaker

--- lib/helpers/classes.tsx
+++ lib/helpers/classes.tsx
@@ -1,7 +1,6 @@
-function classes (...names: (string | undefined)[]) {
+export default function classes (...names: (string | undefined)[]) {
   return names.filter(Boolean).join(' ')
 }
-export default classes
 
 interface Options {
   extra: string | undefined
@@ -9,19 +8,12 @@ interface Options {
 interface ClassToggles {
   [k: string]: boolean
 }
-export function prefixFunctionMaker (prefix = '') {
-  return function (name: string | ClassToggles, options?: Options) {
-    const name2 = (typeof name === 'string' || typeof name === 'undefined') ? { [name]: name } : name
-    const scoped = Object
-      .entries(name2)
+export const prefixFunctionMaker = (prefix = '') =>
+  (name: string | ClassToggles, options?: Options) =>
+    Object
+      .entries(name instanceof Object ? name : { [name]: name })
       .filter(kv => kv[1] !== false)
       .map(kv => kv[0])
       .map(name => [prefix, name].filter(Boolean).join('-'))
+      .concat(options && options.extra || [])
       .join(' ')
-    if (options) {
-      return [scoped, options.extra].filter(Boolean).join(' ')
-    } else {
-      return scoped
-    }
-  }
-}
