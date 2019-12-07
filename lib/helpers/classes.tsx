fa450adb9d53010fc2ebe8666c0f77b7a426f07f
Leioy <13902303081@139.com>
2019-12-07 20:19:05 +0800

    重构 prefixFunctionMaker

--- lib/helpers/classes.tsx
+++ lib/helpers/classes.tsx
@@ -10,22 +10,18 @@ interface ClassToggles {
   [k: string]: boolean
 }
 export function prefixFunctionMaker (prefix = '') {
-  return function (name?: string | ClassToggles, options?: Options) {
-    let name2
-    let result
-    if (typeof name === 'string' || typeof name === 'undefined') {
-      name2 = name
-      result = [prefix, name2].filter(Boolean).join('-')
-    } else {
-      name2 = Object.entries(name).filter(kv => kv[1]).map(kv => kv[0])
-      result = name2.map(n => {
-        return [prefix, n].filter(Boolean).join('-')
-      }).join(' ')
-    }
+  return function (name: string | ClassToggles, options?: Options) {
+    const name2 = (typeof name === 'string' || typeof name === 'undefined') ? { [name]: name } : name
+    const scoped = Object
+      .entries(name2)
+      .filter(kv => kv[1] !== false)
+      .map(kv => kv[0])
+      .map(name => [prefix, name].filter(Boolean).join('-'))
+      .join(' ')
     if (options) {
-      return [result, options.extra].filter(Boolean).join(' ')
+      return [scoped, options.extra].filter(Boolean).join(' ')
     } else {
-      return result
+      return scoped
     }
   }
 }
