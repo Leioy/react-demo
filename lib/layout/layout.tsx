92cea6a92532379375e1a200f1f74f1925921927
Leioy <13902303081@139.com>
2019-12-07 22:01:20 +0800

    fix layout导出

--- lib/layout/layout.tsx
+++ lib/layout/layout.tsx
@@ -19,3 +19,8 @@ const Layout: React.FC<IProps> = (props) => {
 };
 
 export default Layout;
+export { Layout }
+export { default as Header } from './header'
+export { default as Content } from './content'
+export { default as Footer } from './footer'
+export { default as Aside } from './aside'
