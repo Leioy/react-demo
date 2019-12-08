a9472cf461f36447582601265364f49317b8d62a
Leioy <13902303081@139.com>
2019-12-08 21:59:23 +0800

    删除其他无用文件

--- lib/icon/icon.example.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Icon from './icon'
-const IconExample: React.FunctionComponent = () => {
-  return <div>
-    <Icon name="alipay" />
-    <Icon name="wechat" />
-  </div>
-}
-export default IconExample
